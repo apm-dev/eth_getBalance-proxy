5077f5fe815aedd764bdf1ce6594f3be4da4673e
Amir Parsa <develop.apm@gmail.com>
2023-06-18 18:30:36 +0330

    Feature: add main.go

--- src/app/main.go
+++ src/app/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/ReneKroon/ttlcache"
+	"github.com/apm-dev/eth_getBalance-proxy/src/common"
+	"github.com/apm-dev/eth_getBalance-proxy/src/config"
+	_nodeRepo "github.com/apm-dev/eth_getBalance-proxy/src/node/data/repo"
+	_proxyService "github.com/apm-dev/eth_getBalance-proxy/src/proxy"
+	"github.com/apm-dev/eth_getBalance-proxy/src/proxy/data/cache"
+	_proxyHttp "github.com/apm-dev/eth_getBalance-proxy/src/proxy/presentation/http"
+	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
+)
 
 func main() {
-	fmt.Println("Hello Proxy")
+	config := config.NewConfig()
+
+	logLevel, err := logrus.ParseLevel(config.App.LogLevel)
+	if err != nil {
+		panic(err)
+	}
+	logrus.SetLevel(logLevel)
+
+	e := echo.New()
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			c.Response().Header().Set("Access-Control-Allow-Origin", "*")
+			return next(c)
+		}
+	})
+
+	nodeRepo := _nodeRepo.NewNodeRepository()
+
+	ttlCache := ttlcache.NewCache()
+	ttlCache.SkipTtlExtensionOnHit(true)
+
+	rpcProxyCache := cache.NewRpcProxyCache(ttlCache)
+
+	rpcProxy := _proxyService.NewRpcProxyService(config, rpcProxyCache, nodeRepo)
+
+	_proxyHttp.NewProxyHandler(e, rpcProxy)
+
+	// Start server
+	go func() {
+		if err := e.Start(fmt.Sprintf(":%d", config.App.WebPort)); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	common.WaitForSignal()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		logrus.Fatal(err)
+	}
 }
